3d6b22bda5f0ef04ef50f036ba1434336f3717a4
justi <justi@DESKTOP-HKO2RG0>
2017-06-20 16:09:37 +0800

    加入專案檔案。

---
+++ Shop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class ShoppingCart
+    {
+        private IEnumerable<Shop.Tests.Book> enumerable;
+
+        public ShoppingCart(IEnumerable<Shop.Tests.Book> enumerable)
+        {
+            this.enumerable = enumerable;
+        }
+
+        public int CalculatePrice()
+        {
+            return 0;
+        }
+    }
+}
---
+++ ShopTests/ShoppingCartTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Tests
+{
+    [TestClass()]
+    public class ShoppingCartTests
+    {
+        [TestMethod()]
+        public void CalculatePriceTest_Buy_Book_Number1_Quantity1_TotalPrice100()
+        {
+            //arrange
+            var shoppingcart = new ShoppingCart(GetBook());
+            List<BuyBook> buybook = new List<BuyBook>()
+            {
+                new BuyBook() { Serial="B001",Name="哈利波特第一集",Price=100,Quantity=1 }
+            };
+            var expected = 100;
+            //act
+            var actual = shoppingcart.CalculatePrice(buybook);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public IEnumerable<Book> GetBook()
+        {
+            return new List<Book>()
+            {
+                new Book() { Serial="B001",Name="哈利波特第一集",Price=100 },
+                new Book() { Serial="B002",Name="哈利波特第二集",Price=100 },
+                new Book() { Serial="B003",Name="哈利波特第三集",Price=100 },
+                new Book() { Serial="B004",Name="哈利波特第四集",Price=100 },
+                new Book() { Serial="B005",Name="哈利波特第五集",Price=100 }
+            };
+        }
+    }
+
+    public class Book
+    {
+        public string Serial { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public int Price { get; set; }
+    }
+
+    public class BuyBook:Book
+    {
+        public int Quantity { get; set; }
+    }
+}
