657f7a9bcc7e39cd59e5d12e79bfad0aed30e5e6
justi <justi@DESKTOP-7RFFG05>
2017-06-21 00:28:40 +0800

    第四個紅燈

--- ShopTests/ShoppingCartTests.cs
+++ ShopTests/ShoppingCartTests.cs
@@ -85,6 +85,27 @@ namespace Shop.Tests
             Assert.AreEqual(expected, actual);
         }
 
+        [TestMethod()]
+        public void CalculatePriceTest_Buy_Book_volume1_Quantity1＿volume2＿Quantity1_volume3_Quantity1＿volume4_Quantity1＿volume5_Quantity1＿TotalPrice357＿discount＿75()
+        {
+            //arrange
+            var shoppingcart = new ShoppingCart(GetBook());
+            List<BuyBook> buybook = new List<BuyBook>()
+            {
+                new BuyBook() { Serial="B001",Name="哈利波特第一集",Price=100,volume=1,Quantity=1 },
+                new BuyBook() { Serial="B002",Name="哈利波特第二集",Price=100,volume=2,Quantity=1 },
+                new BuyBook() { Serial="B003",Name="哈利波特第三集",Price=100,volume=3,Quantity=1 },
+                new BuyBook() { Serial="B004",Name="哈利波特第四集",Price=100,volume=4,Quantity=1 },
+                new BuyBook() { Serial="B005",Name="哈利波特第五集",Price=100,volume=5,Quantity=1 }
+            };
+            double expected = 357;
+            //act
+            double actual = shoppingcart.CalculatePrice(buybook);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
         public IEnumerable<Book> GetBook()
         {
             return new List<Book>()
